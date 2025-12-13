0b746f7c2bd5d6b945fb1a3ba0d1164200fbb0af
UDatt28-11 <datutdph49742@gmail.com>
2025-12-13 21:22:07 +0700

    forgot password

--- app/Http/Controllers/User/ResetPasswordController.php
+++ app/Http/Controllers/User/ResetPasswordController.php
@@ -7,11 +7,25 @@ use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
-    public function showResetForm($token)
+    /**
+     * Hiển thị form reset password - Redirect về frontend
+     * 
+     * @param Request $request
+     * @param string $token
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function showResetForm(Request $request, $token)
     {
-        return response()->json([
-            'message' => 'Hiển thị form reset password',
-            'token' => $token,
-        ]);
+        $email = $request->query('email');
+        
+        // Redirect về frontend với token và email
+        $frontendUrl = config('app.frontend_url', env('FRONTEND_URL', 'http://localhost:5173'));
+        
+        if ($email) {
+            return redirect($frontendUrl . '/reset-password/' . $token . '?email=' . urlencode($email));
+        } else {
+            // Nếu không có email, vẫn redirect nhưng frontend sẽ yêu cầu nhập email
+            return redirect($frontendUrl . '/reset-password/' . $token);
+        }
     }
 }
